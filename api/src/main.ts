6200549e5c0a099cb3218cc523744fe7b709e5fe
Rushabh Mehta <rushabh.r.mehta@gmail.com>
2026-04-29 15:54:35 +0100

    updated dockerfiles and deploy

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,9 +5,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL ?? '*',
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
